e623ef33081d4abe8686f3d94fe742e7a0df6305
Steven van der Merwe <stevenvdmerwe@gmail.com>
2018-09-24 13:50:35 +0200

    Initial Commit

---
+++ DaniDb/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DaniDb
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var ctx = new DaniContext())
+            {
+                var savedApplicants = ctx.Applicants.Count();
+                Console.WriteLine(savedApplicants);
+                
+                AddApplicantWithJobs(ctx);
+
+                FindApplicantByJob(ctx);
+            } 
+            
+        }
+
+        private static void FindApplicantByJob(DaniContext ctx)
+        {
+            var jobId = 2;
+
+            var applicants = ctx.JobListings.Where(p => p.JobID == jobId).SelectMany(x => x.Applicants);
+            
+            Console.WriteLine(String.Join(",",applicants.Select(p=>p.Name)));
+            
+        }
+
+        private static void AddApplicantWithJobs(DaniContext ctx)
+        {
+            var applicant = new Applicant
+            {
+                Name = "Steven van der Merwe"
+            };
+
+            var jobListing1 = new JobListing {JobTitle = "Job1"};
+            var jobListing2 = new JobListing {JobTitle = "Job2"};
+
+            applicant.JobListings.Add(jobListing1);
+            applicant.JobListings.Add(jobListing2);
+
+            ctx.Applicants.Add(applicant);
+            ctx.SaveChanges();
+        }
+    }
+
+    public class DaniContext : DbContext
+    {
+        public DbSet<Applicant> Applicants { get; set; }
+        public DbSet<JobListing> JobListings { get; set; }
+        
+        public DaniContext() : base("Dani")
+        { }
+    }
+    
+    [Table("Applicant")]
+    public class Applicant
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        public virtual ICollection<JobListing> JobListings { get; set; }
+        
+        public Applicant()
+        {
+            JobListings = new List<JobListing>();
+        }
+    }
+
+    [Table("JobListing")]
+    public class JobListing
+    {
+        [Key]
+        public int JobID { get; set; }
+        [Required]
+        public string JobTitle { get; set; }
+        
+        public virtual ICollection<Applicant> Applicants { get; set; }
+        
+        public JobListing()
+        {
+            Applicants = new List<Applicant>();
+        }
+    }
+    
+    
+}
---
+++ DaniDb/createTables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Applicants (
+  ID integer PRIMARY KEY,
+  Name text NOT NULL
+);
+CREATE TABLE JobListings (
+  JobId integer PRIMARY KEY,
+  JobTitle text NOT NULL
+);
+
